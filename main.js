b58d46d1c111f4b460f2d01de08435bbc0c8481a
mrzayev_abdulloh <abdullohmirzayev841@gmail.com>
2025-04-08 10:54:43 +0500

    salom

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-// Barcha showcase_mark elementlarini tanlash
 let showcase_marks = document.querySelectorAll(".showcase_mark");
-
-// Har bir showcase_mark elementiga hodisa qo'shish
 showcase_marks.forEach((mark) => {
     mark.addEventListener("click", () => {
-        // Tegishli showcase_elementsni yashirish
-        let showcase_element = mark.closest('div'); // closest yordamida ota divni olish
+        let showcase_element = mark.closest('div');
         showcase_element.classList.add("hidden");
     });
 });
+
+
+// main.js
+const apiURL = "https://67e66e836530dbd3110ff49a.mockapi.io/todo/products";
+const container = document.getElementById("product-container");
+
+fetch(apiURL)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(product => {
+      container.innerHTML += `
+        <div class="product">
+          <img src="${product.image_url}" alt="${product.title}" />
+          <h3>${product.title}</h3>
+          <p>Price: $${product.price}</p>
+        </div>
+      `;
+    });
+  });
